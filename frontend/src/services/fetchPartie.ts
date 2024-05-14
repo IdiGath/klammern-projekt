1552787797520f3fd5d51652bbf86fffbe1b454e
IdiGath <ievgenii.izrailtenko@gmail.com>
2024-05-14 10:44:25 +0200

    Funktionierender axios Aufruf

---
+++ frontend/src/services/fetchPartie.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import type Partie from "@/types/Partie";
+
+const apiClient = axios.create({
+        baseURL: "backend/klammern",
+        withCredentials: false,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        }
+    }
+);
+
+export default async function fetchPartie(): Promise<Partie> {
+    console.log("fetchPartie ist aufgerufen")
+    const response = await apiClient.get<Partie>('/partie')
+    console.log(response.data)
+    return response.data;
+}
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Gefetchte-Daten:</h1>
+    <h2>Daten: {{ partie }}</h2>
   </div>
 </template>
+
+<script lang="ts" setup>
+import {reactive} from "vue";
+import type Partie from "@/types/Partie";
+import fetchPartie from "@/services/fetchPartie";
+import {Farbe} from "@/types/Farbe";
+import {Wert} from "@/types/Wert";
+import type {Stand} from "@/types/Stand";
+import type {Runde} from "@/types/Runde";
+import type {Karte} from "@/types/Karte";
+
+const emptyTrumpf: Karte = {farbe: Farbe.EMPTY, wert: Wert.EMPTY};
+const emptyStand: Stand = {punkte: new Map(), augen: new Map(), historie: []};
+const emptyRunde: Runde = {reihenfolge: '', trumpf: emptyTrumpf, karten: new Map()};
+const partie = reactive<Partie>({
+  runde: emptyRunde, stand: emptyStand,
+});
+
+(async () => {
+  Object.assign(partie, await fetchPartie());
+})();
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
