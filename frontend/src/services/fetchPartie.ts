c34c9c2a6da6836a6e364bc8e7ced29cba697120
IdiGath <ievgenii.izrailtenko@gmail.com>
2024-05-16 15:29:31 +0200

    Single quotes für das Projekt aktiviert

--- frontend/src/services/fetchPartie.ts
+++ frontend/src/services/fetchPartie.ts
@@ -1,21 +1,21 @@
-import axios from "axios";
-import type Partie from "@/types/Partie";
+import axios from 'axios'
+import type Partie from '@/types/Partie'
 
-const baseURL = 'http://localhost:8080/api/klammern';
-const developmentBaseURL = 'backend/klammern';
+const baseURL = 'http://localhost:8080/api/klammern'
+const developmentBaseURL = 'backend/klammern'
 
 const apiClient = axios.create({
-        baseURL: import.meta.env.DEV ? developmentBaseURL : baseURL,
-        withCredentials: false,
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        }
+    baseURL: import.meta.env.DEV ? developmentBaseURL : baseURL,
+    withCredentials: false,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
     }
-);
+  }
+)
 
 
 export default async function fetchPartie(): Promise<Partie> {
-    const response = await apiClient.get<Partie>('/partie')
-    return response.data;
+  const response = await apiClient.get<Partie>('/partie')
+  return response.data
 }
