1fffb2704fabb947ad575288bd7c65bfd8219ea1
IdiGath <ievgenii.izrailtenko@gmail.com>
2024-10-12 16:24:41 +0200

    Anpassung vom Javadoc

--- backend/src/main/java/de/idigath/klammern/backend/web/dto/KarteDto.java
+++ backend/src/main/java/de/idigath/klammern/backend/web/dto/KarteDto.java
@@ -2,8 +2,12 @@ package de.idigath.klammern.backend.web.dto;
 
 /**
  * Das Record ist ein Transferobjekt für die Spielkarte.
+ * <p>
+ * Die Klasse verwendet String-Werte, um die Abhängigkeiten von Model-Package zu vermeiden.
  *
  * @see de.idigath.klammern.backend.model.Karte
+ * @see de.idigath.klammern.backend.model.Farbe
+ * @see de.idigath.klammern.backend.model.Wert
  */
 public record KarteDto(String farbe, String wert) {
 }
