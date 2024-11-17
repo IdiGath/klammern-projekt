27b83414fd854983b6f991c40f0f462fa975b8df
IdiGath <ievgenii.izrailtenko@gmail.com>
2024-11-17 19:05:56 +0100

    Wechsel der Trumpfkarte

--- backend/src/main/java/de/idigath/klammern/backend/service/spiel/Ansage.java
+++ backend/src/main/java/de/idigath/klammern/backend/service/spiel/Ansage.java
@@ -1,11 +1,13 @@
 package de.idigath.klammern.backend.service.spiel;
 
+import de.idigath.klammern.backend.model.Karte;
 import de.idigath.klammern.backend.model.PhasenInfo;
+import de.idigath.klammern.backend.model.Wert;
 import de.idigath.klammern.backend.model.Zug;
 
 
 public class Ansage extends AbstractPhase implements Phase {
-    private boolean nativeTrumpf;
+    private final boolean nativeTrumpf;
     private boolean ansageFertig;
 
     public Ansage(PhasenInfo phasenInfo) {
@@ -21,12 +23,28 @@ public class Ansage extends AbstractPhase implements Phase {
     @Override
     public void spieleZug(Zug zug) {
         validateZug(zug);
-        isTrumpfkarteWechsel();
+        if (isTrumpfkarteWechsel(zug)) {
+            trumpfWechseln(zug);
+        } else {
+            validateAnsage();
+            ansageFertig = true;
+        }
     }
 
-    private void isTrumpfkarteWechsel() {
+    private void validateAnsage() {
+    }
 
+    private void trumpfWechseln(Zug zug) {
+        var reihe = reihen.get(zug.getBeginner());
+        reihe.addSpielkarte(trumpfKarte);
+        trumpfKarte = zug.getBeginnerKarten().getFirst();
+    }
 
+    private boolean isTrumpfkarteWechsel(Zug zug) {
+        return zug.getBeginnerKarten().size() == 1
+                && zug.getDeckerKarten().isEmpty()
+                && zug.getBeginnerKarten().getFirst().equals(new Karte(trumpfKarte.farbe(), Wert.SIEBEN))
+                && nativeTrumpf;
     }
 
     @Override
